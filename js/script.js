68c9f9b4ba691580dcb556f2f943848b6c5551c2
adele <adele.r87@gmail.com>
2021-01-08 16:46:11 +0100

    modifiche

--- js/script.js
+++ js/script.js
@@ -14,9 +14,9 @@
 // Le validazioni e i controlli possiamo farli anche in un secondo momento.
 // Ricordatevi che se non sappiamo quante volte dobbiamo fare una cosa ci serve…
 
-// step 1  --- genero un numero casualeda 1 a 100
+// step 1  --- genero un numero casuale da 1 a 100
 
-// FUNZIONE 1
+// FUNZIONE 1 GENERA NUMERI RANDOM
 
 var min = 1;
 var max = 100;
@@ -25,7 +25,19 @@ function randomNum(min,max) {
     return Math.floor(Math.random() * (max + 1 - min) + min)
  }
 
- var randomNumber = randomNum(min,max)
+ // FUNZIONE 2 MI DICE SE HO GIA INSERITO UN NUMERO IN UN CICLO FOR
+
+ function insertNum(insertNumber) {
+   if (insertNumber === num) {
+     return true;
+   }else {
+     resturn false
+   }
+ }
+ var numeroinserito = insertNum(insertNumber)
+
+
+// / var randomNumber = randomNum(min,max) questa se la richiamo dentro un ciclo avra sempre quel valore cioe di un solo numero invece se la metto come var randomNum ecc mi ciclerà sempre un numero differente
 
  // cosi me ne stampa uno alla volta io ne voglio 16 casuali
 
@@ -33,21 +45,52 @@ function randomNum(min,max) {
  // step 2  --- voglio prendere 16 numeri  sempre casuali che mi sta generando la funzione sempre tra qui 100 e inserirli in un array ( vuoto)
 
 // CICLO FOR O WHILE
+// il ciclo while cicla fino a quando le condizioni non sono verificate in qeusto caso se gli diciamo che la lunghezza dell array deve essere di 16 ciclirà fino a quando non avra sedici numeri al suo interno
 
  var arrayNum = []
  var howManyNumber = 16
- for (var i = 0; i < howManyNumber; i++) {
 
-   if (randomNumber !== arrayNum[i]) {
-    arrayNum.push(randomNumber)
-   }
+ while ( arrayNum.length < howManyNumber) {
+   var randomNumber = randomNum(min,max)
 
-  }
+  if ( !arrayNum.includes(randomNumber)) {
+ arrayNum.push(randomNumber)
+}
+}
   console.log(arrayNum);
 
-  // STEP 2 --- chiedo all utente di inserire 84 volte un numero
+  // STEP 2 --- chiedo all utente di inserire 3 volte un numero sapendo che
+  // L’utente non può inserire più volte lo stesso numero.
 
-  // var howManyPrompt = 84
-  // for (var i = 0; i < howManyPrompt; i++) {
-  //   var num = parseInt(prompt('inserisci numero'))
+  // Se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti si continua chiedendo all’utente un altro numero.
+
+  // La partita termina quando il giocatore inserisce un numero “vietato” o raggiunge il numero massimo possibile di numeri consentiti.
+
+  var howManyPrompt = 3
+
+  // while (howManyPrompt ) {
+  //   var num = parseInt(prompt('inserisci numero da 1 a 100'))
+  //
+  //   if (num === arrayNum) {
+  //     alert('termina gioco')
+  //   }else if (num === num) {
+  //     alert('hai gia inserito questo numero')
+  //   } else{
+  //     alert('continua')
+  //   }
   // }
+
+  for (var i = 0; i < howManyPrompt; i++){
+    var num = parseInt(prompt('inserisci numero da 1 a 100'))
+
+    if (numeroinserito) {
+      alert('numero gia inserito')
+    } else if (num === arrayNum[i]) {
+      alert('termina gioco')
+    } else {
+      alert('calcoliamo il punteggio')
+    }
+
+    }
+
+  }
